e3f74b460c9c148fd440ff96d200854119fb6d3c
NatnaelWale <nhattyjeje@gmail.com>
2024-04-30 13:43:23 -0400

    Finished the exercise

---
+++ Functions/simple_functions.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+function favouriteThings() {
+    console.log("Natnael\nWalking Dead\nKanye West");
+};
+
+function whyImHere() {
+    console.log("To grasp a coding experiance!");
+};
+
+function favouritePlace() {
+    console.log("Lalibela - as it is one of an amazing building piece made from a single unit of stone.");
+};
+
+favouriteThings();
+
+whyImHere();
+
+favouritePlace();
