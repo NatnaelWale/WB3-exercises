b4b1cc27b20acb5403a238ee9052ad0e87171460
NatnaelWale <nhattyjeje@gmail.com>
2024-05-01 10:31:43 -0400

    Did scoping exercise

---
+++ Functions/scopeExercises.js
@@ -0,0 +1,74 @@
+"use strict";
+
+// function test1() {
+//   let a = 10;
+//   if (a > 5) {
+//     a = 7;
+//   }
+//    console.log("a = " + a); // a = 7
+// }
+// test1();
+
+
+// function test2A() {
+//   if (1 == 1) {
+//     var a = 5;
+//   }
+//   console.log("a = " + a); // a = 5
+// }
+// test2A();
+
+
+// function test2B() {
+//  if (1 == 1) {
+//  let a = 5;
+//  }
+//  console.log("a = " + a); // a is not defined
+// }
+// test2B();
+
+
+// let a = 4;
+// function test3() {
+//  a = 3;
+//  console.log("a = " + a); // a = 3
+// }
+// test3();
+// console.log("a = " + a); // a = 3
+
+
+// let a = 4;
+// function test4() {
+//  let a = 7;
+//  console.log("a = " + a); // a = 7
+// }
+// test4();
+// console.log("a = " + a); // a = 4
+
+
+// let a = 4;
+// function test5() {
+//  a = 7;
+//  function again() {
+//  let a = 8;
+//  console.log("a = " + a);  // a = 8
+//  }
+//  again();
+//  console.log("a = " + a); // a = 7
+// }
+// test5();
+// console.log("a = " + a); // a = 7
+
+// let a = 4;
+// function test6() {
+//  let a = 7;
+//  function again() {
+//  let a = 8;
+//  console.log("a = " + a); // a = 8
+//  }
+//  again();
+//  console.log("a = " + a);  // a = 7
+// }
+// test6();
+// console.log("a = " + a); // a = 4
+
